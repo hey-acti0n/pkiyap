eb55407ca128a6297f2c21f7ae05cb59908d5101
action <alexemel2@yandex.ru>
2024-11-05 23:04:48 +0300

    rk1

---
+++ RK1.py
@@ -0,0 +1,70 @@
+class Student:
+    def __init__(self, student_id, surname, scholarship, group_id):
+        self.student_id = student_id
+        self.surname = surname
+        self.scholarship = scholarship
+        self.group_id = group_id  
+
+class Group:
+    def __init__(self, group_id, group_name):
+        self.group_id = group_id
+        self.group_name = group_name
+
+class GroupStudent:
+    def __init__(self, student_id, group_id):
+        self.student_id = student_id
+        self.group_id = group_id
+
+
+
+students = [
+    Student(student_id=1, surname="Андреев", scholarship=1500, group_id=1),
+    Student(student_id=2, surname="Борисов", scholarship=1200, group_id=1),
+    Student(student_id=3, surname="Алексеева", scholarship=1800, group_id=2),
+    Student(student_id=4, surname="Аркадьев", scholarship=1600, group_id=3),
+    Student(student_id=5, surname="Васильева", scholarship=1100, group_id=2)
+]
+
+
+groups = [
+    Group(group_id=1, group_name="Группа А"),
+    Group(group_id=2, group_name="Группа Б"),
+    Group(group_id=3, group_name="Группа В")
+]
+
+
+group_students = [
+    GroupStudent(student_id=1, group_id=1),
+    GroupStudent(student_id=2, group_id=1),
+    GroupStudent(student_id=3, group_id=2),
+    GroupStudent(student_id=4, group_id=3),
+    GroupStudent(student_id=5, group_id=2)
+]
+
+
+result_1 = [
+    (student.surname, next(group.group_name for group in groups if group.group_id == student.group_id))
+    for student in students if student.surname.startswith("А")
+]
+
+group_min_scholarships = {
+    group.group_name: min(student.scholarship for student in students if student.group_id == group.group_id)
+    for group in groups
+}
+result_2 = sorted(group_min_scholarships.items(), key=lambda x: x[1])
+
+result_3 = sorted([
+    (next(student.surname for student in students if student.student_id == gs.student_id),
+     next(group.group_name for group in groups if group.group_id == gs.group_id))
+    for gs in group_students
+], key=lambda x: x[0])
+
+print("Task#1")
+for a in result_1:
+    print(a)
+print("Task#2")
+for a in result_2:
+    print(a)
+print("Task#3")
+for a in result_3:
+    print(a)
